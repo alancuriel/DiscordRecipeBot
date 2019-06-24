3da0593842d31385495d53322365fa541b68cb07
Alan Curiel <alannitro17@gmail.com>
2019-06-24 13:17:17 -0500

    Add Dependancy Injection

---
+++ BotCore/Unity.cs
@@ -0,0 +1,28 @@
+using Unity;
+
+namespace BotCore
+{
+    public static class Unity
+    {
+        private static UnityContainer _container;
+
+        public static UnityContainer UnityContainer
+        {
+            get
+            {
+                if(_container == null)
+                    RegisterTypes();
+                
+                return _container;
+            }
+        }
+
+        public static void RegisterTypes()
+        {
+            _container = new UnityContainer();
+            //_container.RegisterType<interface, implemtation>();
+            //_container.RegisterSingleton<interface, implemtation>();
+        }
+
+    }
+}
