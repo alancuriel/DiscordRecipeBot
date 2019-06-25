17f85c0b344f7448dc9aed5813da17b80a1a6bf8
Alan Curiel <alannitro17@gmail.com>
2019-06-25 14:01:18 -0500

    Add basic ping module
    Added a simple reply to a ping

---
+++ BotCore/Discord/Modules/PingModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCore.Discord.Modules
+{
+    public class PingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
